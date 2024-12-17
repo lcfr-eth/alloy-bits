5d7ac19e65256de137c9fcd1d35ac323b21a0afb
Your Name <me@lcfr.io>
2024-12-16 22:47:05 -0500

    a

--- stream-mempool/src/main.rs
+++ stream-mempool/src/main.rs
@@ -1,12 +1,18 @@
-
-use alloy_provider::{network::AnyNetwork, Provider, ProviderBuilder, RootProvider};
-use alloy_rpc_client::WsConnect;
 use eyre::Result;
 use futures_util::StreamExt;
 
+use alloy::{
+    network::TransactionBuilder,
+    primitives::{address, U256},
+    providers::{ext::TraceApi, ProviderBuilder},
+    rpc::types::{trace::parity::TraceType, TransactionRequest, Transaction},
+};
+use alloy::providers::Provider;
+use alloy::providers::WsConnect;
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let rpc_url = "ws://10.0.0.227:8546";
+    let rpc_url = "ws://127.0.0.1:8546";
 
     let ws = WsConnect::new(rpc_url);
     let provider = ProviderBuilder::new().on_ws(ws).await?;
@@ -14,14 +20,24 @@ async fn main() -> Result<()> {
     let sub = provider.subscribe_full_pending_transactions().await?;
 
     // Wait and take the next 3 transactions.
-    let mut stream = sub.into_stream();
+    //let mut stream = sub.into_stream();
+    let mut stream = sub.into_stream().take(1);
 
     println!("Awaiting pending transactions...");
 
     let handle = tokio::spawn(async move {
         while let Some(tx) = stream.next().await {
-            println!("{:#?}", tx);
-        }
+            //println!("{:#?}", tx);
+
+	    //let tx = TransactionRequest::default().from(tx.from).to(tx.transact_to).input(calldata.into());
+
+            //TransactionRequest::default().with_from(tx.from).with_to(tx.transact_to).with_data(tx.data.0);
+
+
+	    let trace_type = [TraceType::Trace];
+            let result = provider.trace_call(&tx.into(), &trace_type).await;
+            println!("{:?}", result.unwrap().trace);
+      }
     });
 
     handle.await?;
